39c57fe411d9e0516371625852eb9a98270f48e0
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2024-07-01 19:57:45 +0100

    Set up deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+};
+
+module.exports = nextConfig;
